65b70d5a412f40ebb80e20c7190c658608ddfd51
LsaBarbosa <l.sant.anna.b@gmail.com>
2025-03-15 10:48:58 -0300

    Crinado pagina de login

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {AuthInterceptor} from './auth.interceptor';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule, // Necessário para utilizar formulários reativos
+    AppComponent,
+    LoginComponent,
+  ],
+
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
+  bootstrap: []
+})
+export class AppModule { }
